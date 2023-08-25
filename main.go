6bca0288e861f406661ac04249416f0dec2894ab
Gaston Valles <gastonvallesyeou@gmail.com>
2023-08-25 04:24:35 -0300

    Tmeplates folder created with Start HTML

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
+
+var templates = template.Must(template.ParseGlob("templates/*"))
 
 func main() {
 	http.HandleFunc("/", Start)
@@ -13,5 +15,5 @@ func main() {
 }
 
 func Start(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Gopher!")
+	templates.ExecuteTemplate(w, "start", nil)
 }
